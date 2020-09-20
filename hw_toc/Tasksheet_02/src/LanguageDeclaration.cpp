c1e1077730c9f07c9967cb5a373681b29b037433
jpoll962 <42617536+jpoll962@users.noreply.github.com>
2020-09-19 22:35:17 -0600

    Creating LanguageDeclaration.cpp

---
+++ hw_toc/Tasksheet_02/src/LanguageDeclaration.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello World! << std::endl;
+  std::cout << "I have chosen to use the C++ Programming Language for this class." << std::endl;
+  return 0;
+}
