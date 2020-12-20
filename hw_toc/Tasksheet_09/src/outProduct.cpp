09541c909f7425cf150a7df9b6cecc97c61f124d
Joe <jpoll962@yahoo.com>
2020-12-20 15:49:46 -0700

    Adding code for Task 1

--- hw_toc/Tasksheet_09/src/outProduct.cpp
+++ hw_toc/Tasksheet_09/src/outProduct.cpp
@@ -1,47 +1,23 @@
 #include <iostream>
 
-
-///////////////////////
-// Working on definition of what Outer Product means
-//
-//
-//
-class Vect
-{
-    private:
-        double one, two, three;
-    public:
-        Vect(double o = 0, double  tw = 0, double th = 0)
-        {
-            one = o;
-            two = tw;
-            three = th;
-        }
-};
-
 int main()
 {
     double matrix[3][3];
-    double vec1[3] = {1,6,2};
-    double vec2[3] = {4,5,9};
+    double vec1[3] = {1,2,3}; // Vertical Matrix
+    double vec2[3] = {3,1,2}; // Horizontal Matrix
 
     for (int i = 0; i < 3; i++)
         {
-            matrix[i][0] = vec1[i];
+            for (int j = 0; j < 3; j++)
+            {
+                matrix[i][j] = vec1[i] * vec2[j]; // Compute values to assign into the matrix
+            }
         }
 
-    for (int i = 0; i < 3; i++)
-        {
-            matrix[0][i] = vec2[i];
-        }
-
-    std::cout << "The Resulting Matrix is ";
+    std::cout << "The Resulting Matrix is: " << std::endl;
     for (int i = 0; i < 3; i++)
     {
-        for (int j = 0; j < 3; j++)
-        {
-            std::cout << matrix[i][j] << std::endl;
-        }
+        std::cout << "| " << matrix[i][0] << " " << matrix[i][1] << " " << matrix[i][2] << " |" << std::endl;   // Print Matrix to screen
     }
 
     return 0;
